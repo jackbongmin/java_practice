04b36b3525f018a8b3b3b11989524a7436885172
jackbongmin <rrrgk12@gmail.com>
2026-08-14 17:47:27 +0900

    feat. string test

--- src/Test/Test0812_01.java
+++ src/Test/Test0812_01.java
@@ -8,11 +8,13 @@ public class Test0812_01 {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("아무거나 입력하시오.");
-        String str1 = sc.nextLine();
-        System.out.print("당신이 입력한 글자는 ");
-        System.out.print(str1);
-        System.out.print("입니다.");
+
+        String str1 = "안녕하세요, ";
+        String str2 = "반갑습니다.";
+
+        System.out.println(str1.concat(str2));
+
+        // 결과 : 안녕하세요, 반갑습니다.
 
     }
 }
